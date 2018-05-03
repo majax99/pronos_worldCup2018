45253950c64651b2cac66c12c95de98b06f5c443
majax99 <pierre-yves.blanc2@orange.fr>
2018-05-03 20:48:48 +0200

    ajout de la partie matchs

--- resources/views/equipes/equipe.blade.php
+++ resources/views/equipes/equipe.blade.php
@@ -2,6 +2,77 @@
 
 @section('content')
 
-    {{ dd($data) }}
+    {{ dd($team) }}
+    <div class="container" id="homepage">
+        <div class = "row" style = "margin-top:50px;">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading panel-info">
+                        <div class = "row">
+                            <div class = "col-md-6 text-center">
+                                <h5><strong>Name :</strong> {{($team[0]->team_name)}} <img  src = "/img/country/{{$team[0]->country}}.png"></h5>
+                                <h5><strong> Country :</strong> {{($team[0]->country)}}</h5>
+                                <h5> <strong>Rank : </strong>{{($team[0]->rank)}}</h5>
+                            </div>
+                            <div class = "col-md-6 text-center">
+                                @php(
+                                $win = rand(31-$team[0]->rank,31)
+                                )
+                                @php(
+              $draw = rand(0,31 - $win)
+               )
+                                @php(
+                                 $lose = 31 - $draw - $win
+                                 )
+                                <h5><strong>Matches Win :</strong> {{$win}} </h5>
+                                <h5><strong> Matches Lose :</strong> {{$draw}}</h5>
+                                <h5> <strong>Matches draw : </strong>{{$lose}}</h5>
+                            </div>
+                        </div>
 
+                    </div>
+                    <div class="panel-heading panel-info">
+                        <h3 class = "text-center">List of players</h3>
+                    </div>
+                    <div class="panel-body">
+                        <div class="row">
+                            <div class="table-responsive">
+                                <table id="tableProno" class="table table-condensed">
+                                    <thead>
+                                    <th style="width:5%;"></th>
+                                    <th class= "text-center" style="vertical-align:middle;">First Name </th>
+                                    <th class= "text-center" style="vertical-align:middle;"> Last name </th>
+                                    <th class= "text-center" style="vertical-align:middle;"> Age  </th>
+                                    <th class= "text-center" style="vertical-align:middle;"> weight </th>
+                                    <th class= "text-center" style="vertical-align:middle;"> height  </th>
+                                    <th class= "text-center" style="vertical-align:middle;"> Captain  </th>
+                                    </thead>
+                                    <tbody>
+                                    @foreach ($team as $team_stat)
+                                        <tr>
+
+                                            <td class = "text-center " style="vertical-align:middle;"><img src = "/img/players/{{$team_stat->firstname}}{{$team_stat->lastname}}.jpg"></td>
+                                            <td class = "text-center" style="vertical-align:middle;"> {{$team_stat->firstname}}                             </td>
+                                            <td class = "text-center" style="vertical-align:middle;"> {{$team_stat->lastname}}                             </td>
+                                            <td class = "text-center" style="vertical-align:middle;"> {{$team_stat->age}}                             </td>
+                                            <td class = "text-center" style="vertical-align:middle;"> {{$team_stat->weight}}                             </td>
+                                            <td class = "text-center" style="vertical-align:middle;"> {{number_format($team_stat->height,2)}}m                             </td>
+                                            @if ( $team_stat->capitain == 1)
+                                                <td class="text-center" style="vertical-align:middle;"><img  src = "/img/players/capitaine.png"></td>
+                                            @else
+                                                <td></td>
+
+                                            @endif
+
+                                        </tr>
+                                    @endforeach
+
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
---
+++ resources/views/matchs/match.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container" id="homepage">
+        <div class = "row" style = "margin-top:50px;">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading panel-info">Matches</div>
+                    @php ($i = 1)
+                    <div class="panel-body">
+                        @foreach ($matches as $match)
+                            @if ( $i <= 2 || $i == 5 || ($i>8 && $i<34 && $i%3 ==0 || $i==37 || $i == 41 || $i == 45))
+                                <div class = "alert alert-success">
+                                    <span>{{ \Carbon\Carbon::parse($match->date_match)->format('l d F Y')}}   </span>
+                                </div>
+                            @endif
+                            <p class='clickable-row' data-href="{{url('matches',['id'=>$i])}}">
+                                @if ($match->assists1 == 0)
+                                    <span class = "pull-left" style = "width:40%;"> {{ \Carbon\Carbon::parse($match->date_match)->format('H:i')}}</span>
+                                @else
+                                    <span class = "pull-left text-success" style = "width:40%;" ><strong>FINISHED</strong> </span>
+                                @endif
+                                <span style= "text-align:center;">
+                  <img  src = "/img/country/{{$match->team1}}.png">{{$match->team1}}
+                                    @if ($match->assists1 == 0)
+                                        VS
+                                    @else
+                                        <strong>{{$match->goals1}} - {{$match->goals2}} </strong>
+                                    @endif
+
+                                    {{$match->team2}}
+                                    <img  src = "/img/country/{{$match->team2}}.png"></span>
+                                <span class = "pull-right">  {{$match->location}}      </span><br>
+                            </p>
+                            @php ($i++)
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script> jQuery(document).ready(function($) {
+            $(".clickable-row").click(function() {
+                window.location = $(this).data("href");
+            });
+            $(".clickable-row").hover(function() {
+                $(this).css( 'cursor', 'pointer' )
+                    .toggleClass("bg-info")
+                    .siblings(".selected")
+                    .removeClass("selected");
+            });
+        });</script>
+@endsection
